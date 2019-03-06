94249f5088fdd367189d9753972e9e0ca9d30a41
MikiKru <michal_kruczkowski@o2.pl>
2019-03-06 11:33:16 +0100

    commit from dev

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// psvm -> public static void main(){}
+// sout -> System.out.println();
+// CTRL + SHIFT + F10 -> run
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
